7efc8b634409e12f42bcda6cf1bd270928966a83
LAVINSE <freedomsewon@gmail.com>
2024-03-05 18:33:34 +0900

    fix
    fix

--- Assets/Test/InventoryItem.cs
+++ Assets/Test/InventoryItem.cs
@@ -6,10 +6,19 @@ using UnityEngine.UI;
 
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    
+
     [Header("UI")]
+    public Image image;
+
+    [HideInInspector] public Item item;
     [HideInInspector] public Transform parentAfterDrag;
 
-    public Image image;
+    public void InitialiseItem(Item newItem)
+    {
+        item = newItem;
+        image.sprite = newItem.image;
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
